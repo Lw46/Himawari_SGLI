472b08f680fa5a1d4ca015839bf4179c5d7cccb8
Lw46 <46706989+Lw46@users.noreply.github.com>
2022-04-20 13:01:22 +0900

    Update USELUT_band03.py

--- USELUT_band03.py
+++ USELUT_band03.py
@@ -241,8 +241,8 @@ fn1 = RegularGridInterpolator((water,ozone,AOT,raa,sza_new,vza_new),X1_new_inter
 fn2 = RegularGridInterpolator((water,ozone,AOT,raa,sza_new,vza_new),X2_new_inter_reshape,bounds_error=False,fill_value=np.nan)  
 fn3 = RegularGridInterpolator((water,ozone,AOT,raa,sza_new,vza_new),X3_new_inter_reshape,bounds_error=False,fill_value=np.nan)
 
-lat_x=np.linspace(50,20,6000)
-lon_y=np.linspace(120,150,6000)
+lat_x=np.linspace(50,20,6001)
+lon_y=np.linspace(120,150,6001)
 
 BAND=Hi8_band()
 
